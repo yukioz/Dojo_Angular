ee8b8443a2ca32bda5cd0b876f4e3b3df8dfd81a
yukioz <yukio.link@gmail.com>
2021-07-07 23:45:21 -0300

    Init pokedex
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- Pokedex/karma.conf.js
+++ Pokedex/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/projeto1'),
+      dir: require('path').join(__dirname, './coverage/Pokedex'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Pokedex/src/app/app.module.ts
+++ Pokedex/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/Components/home.component';
+
+// Componentss
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
